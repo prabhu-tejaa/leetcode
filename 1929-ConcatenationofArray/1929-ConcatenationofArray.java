8afb660f3b3b70cea1b524c6cbed0bd70cffff61
Prabhu Teja Pamula <94112233+teja137@users.noreply.github.com>
2024-12-13 11:54:05 +0530

    Update file 12/13/2024, 11:54:03 AM

--- 1929-ConcatenationofArray/1929-ConcatenationofArray.java
+++ 1929-ConcatenationofArray/1929-ConcatenationofArray.java
@@ -1,12 +1,11 @@
 class Solution {
     public int[] getConcatenation(int[] nums) {
-        int lengthOfArray = nums.length;
-        int[] answer = new int[lengthOfArray*2];
-        for(int i = 0;i<lengthOfArray;i++){
-            answer[i]=nums[i];
-            answer[i+lengthOfArray]=nums[i];
-
+        int numsSize = nums.length;
+        int[] ans = new int[numsSize*2];
+        for(int i = 0; i<numsSize; i++){
+            ans[i] = nums[i];
+            ans[i+numsSize] = nums[i];
         }
-        return answer;
+        return ans;
     }
 }
