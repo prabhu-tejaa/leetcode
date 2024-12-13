873ba26b115d34c4e2958398383ac5a673d1b4aa
Prabhu Teja Pamula <94112233+teja137@users.noreply.github.com>
2024-12-13 14:38:57 +0530

    Update file 12/13/2024, 2:38:57 PM

--- 1365-HowManyNumbersAreSmallerThantheCurrentNumber/1365-HowManyNumbersAreSmallerThantheCurrentNumber.java
+++ 1365-HowManyNumbersAreSmallerThantheCurrentNumber/1365-HowManyNumbersAreSmallerThantheCurrentNumber.java
@@ -2,7 +2,6 @@ class Solution {
     public int[] smallerNumbersThanCurrent(int[] nums) {
         int numsSize = nums.length;
         int[] answer = new int[numsSize];
-        int low = lower(nums);
         for(int i = 0;i < numsSize; i++){
             for(int j = 0; j< numsSize;j++){
                 if(nums[i] > nums[j]){
@@ -11,15 +10,5 @@ class Solution {
             }
         }
         return answer;
-    }
-    static int lower(int [] arr){
-        int lowestValue = arr[0];
-        for(int i = 1; i< arr.length;i++){
-            if(arr[i] < lowestValue){
-                lowestValue = arr[i];
-            }
-        }
-        return lowestValue;
-
     }
 }
