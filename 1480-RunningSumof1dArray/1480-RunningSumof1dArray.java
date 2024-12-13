5e414b2219b0340b9cc279ec15de17ec9d019cf4
Prabhu Teja Pamula <94112233+teja137@users.noreply.github.com>
2024-12-13 12:04:58 +0530

    Update file 12/13/2024, 12:04:56 PM

--- 1480-RunningSumof1dArray/1480-RunningSumof1dArray.java
+++ 1480-RunningSumof1dArray/1480-RunningSumof1dArray.java
@@ -1,7 +1,8 @@
 class Solution {
     public int[] runningSum(int[] nums) {
-        for(int i =1; i< nums.length;i++){
-            nums[i] += nums[i-1];
+        int numsSize = nums.length;
+        for(int i = 1; i< numsSize; i++){
+            nums[i] = nums[i]+nums[i-1];
         }
         return nums;
     }
