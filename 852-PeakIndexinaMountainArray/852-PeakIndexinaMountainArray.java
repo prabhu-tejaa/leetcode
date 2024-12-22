6699efe0ab380480b305cd8c4dc53bbe76657f14
Prabhu Teja Pamula <94112233+teja137@users.noreply.github.com>
2024-12-22 17:31:52 +0530

    Create file

---
+++ 852-PeakIndexinaMountainArray/852-PeakIndexinaMountainArray.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int peakIndexInMountainArray(int[] arr) {
+        int sizeOfArr = arr.length;
+        int start = 0;
+        int end = sizeOfArr - 1;
+        while(start<end){
+            int mid = start + (end-start)/2;
+            if(arr[mid] > arr[mid+1]){
+                end = mid;
+            } else {
+                start =  mid + 1;
+            }
+        }
+        return start;
+    }
+}
