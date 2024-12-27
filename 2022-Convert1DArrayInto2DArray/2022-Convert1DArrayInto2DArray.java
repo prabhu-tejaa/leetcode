0417f7358278f2ac353d86d43bc2aafe9647d5c9
Prabhu Teja Pamula <94112233+prabhutejapamula@users.noreply.github.com>
2024-12-27 21:24:55 +0530

    Create file

---
+++ 2022-Convert1DArrayInto2DArray/2022-Convert1DArrayInto2DArray.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int[][] construct2DArray(int[] original, int m, int n) {
+        int[][] result = new int[m][n];
+
+        if (original.length != m * n) {
+            return new int[0][0];
+        }
+
+         for (int i = 0; i < original.length; i++) {
+            result[i / n][i % n] = original[i];
+        }
+        
+        return result;
+    }
+}
