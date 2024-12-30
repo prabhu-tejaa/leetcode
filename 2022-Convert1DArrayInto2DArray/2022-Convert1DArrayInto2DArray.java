9c2fab43f5cf443f5559a260f0e4e25e9ea89898
Prabhu Teja Pamula <94112233+prabhutejapamula@users.noreply.github.com>
2024-12-31 00:29:58 +0530

    Update file 12/31/2024, 12:29:59 AM

--- 2022-Convert1DArrayInto2DArray/2022-Convert1DArrayInto2DArray.java
+++ 2022-Convert1DArrayInto2DArray/2022-Convert1DArrayInto2DArray.java
@@ -1,14 +1,15 @@
 class Solution {
     public int[][] construct2DArray(int[] original, int m, int n) {
-        int[][] answer = new int[m][n];
         if(original.length != m*n){
             return new int[0][0];
         }
-        int originalSize = original.length;
-        int index = 0;
-        for(int i = 0; i<m; i++){
-            for(int j = 0; j<n; j++){
-                answer[i][j] = original[index++];
+        int[][] answer = new int [m][n];
+        int i=0, j = 0;
+        for(int nums:original){
+            answer[i][j++]=nums;
+            if(j>=n){
+                j=0;
+                i++;
             }
         }
         return answer;
