890466a5974f90b7fe2f0a896aa88e9528762694
Prabhu Teja Pamula <94112233+prabhutejapamula@users.noreply.github.com>
2025-04-03 11:25:20 +0530

    File updated at 4/3/2025, 11:25:19 AM

--- 242-ValidAnagram/242-ValidAnagram.java
+++ 242-ValidAnagram/242-ValidAnagram.java
@@ -1,23 +1,27 @@
+// Last updated: 4/3/2025, 11:25:19 AM
 class Solution {
     public boolean isAnagram(String s, String t) {
-        int sLength = s.length();
-        int tLength = t.length();
-        HashMap<Character, Integer> freqMap = new HashMap<>();
-        if(sLength != tLength){
+        Map<Character, Integer> map = new HashMap<>();
+        int sSize = s.length();
+        int tSize = t.length();
+        int initial = 1;
+
+        if(sSize != tSize){
             return false;
-        };
-        for (int i = 0; i < s.length(); i++) {
-            char ch1 = s.charAt(i);
-            char ch2 = t.charAt(i);
-
-            freqMap.put(ch1, freqMap.getOrDefault(ch1, 0) + 1);
-            freqMap.put(ch2, freqMap.getOrDefault(ch2, 0) - 1);
         }
 
-        for (int value : freqMap.values()) {
-            if (value != 0) return false;
+        for(int i = 0; i<sSize;i++){
+            char ch = s.charAt(i);
+            map.put(ch, map.getOrDefault(ch,0)+1);
         }
 
-        return true;
+        for(int i = 0;i<tSize;i++){
+            char ch= t.charAt(i);
+            if(!map.containsKey(ch) || map.get(ch)==0){
+                return false;
+            }
+            map.put(ch,map.get(ch)-1);
         }
+    return true;
+    }
 }
