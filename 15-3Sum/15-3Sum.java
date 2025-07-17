4bbb5236c5f749c473ebdd3367f487ef1f652e43
Prabhu Teja <94112233+prabhu-tejaa@users.noreply.github.com>
2025-07-17 23:17:59 +0530

    File updated at 7/17/2025, 11:17:58 PM

--- 15-3Sum/15-3Sum.java
+++ 15-3Sum/15-3Sum.java
@@ -1,36 +1,30 @@
-// Last updated: 7/17/2025, 11:17:01 PM
+// Last updated: 7/17/2025, 11:17:58 PM
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
-      List<List<Integer>> result = new ArrayList<>();
-        Arrays.sort(nums);  // Sort the array
+        Set<List<Integer>> result = new HashSet<>();
+        int numsLength = nums.length;
 
-        for (int i = 0; i < nums.length - 2; i++) {
-            // Skip duplicate values for the first number
-            if (i > 0 && nums[i] == nums[i - 1]) continue;
+        Arrays.sort(nums);
 
-            int target = -nums[i];
-            int left = i + 1, right = nums.length - 1;
+        for(int i = 0; i < numsLength - 2; i++){
 
-            while (left < right) {
-                int sum = nums[left] + nums[right];
+            int left = i + 1;
+            int right = numsLength - 1;
 
-                if (sum == target) {
+            while(left < right){
+                int sum = nums[i] + nums[left] + nums[right];
+
+                if(sum == 0){
                     result.add(Arrays.asList(nums[i], nums[left], nums[right]));
                     left++;
                     right--;
-
-                    // Skip duplicate values for second and third numbers
-                    while (left < right && nums[left] == nums[left - 1]) left++;
-                    while (left < right && nums[right] == nums[right + 1]) right--;
-
-                } else if (sum < target) {
-                    left++;
-                } else {
+                }else if(sum > 0){
                     right--;
+                } else {
+                    left++;
                 }
             }
         }
-
-        return result;
+        return new ArrayList<>(result);
     }
 }
