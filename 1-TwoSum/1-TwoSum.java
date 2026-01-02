19c841ce5652603e2dc9f7e376ac474d57fa8c24
Prabhu Teja <94112233+prabhu-tejaa@users.noreply.github.com>
2026-01-02 21:14:33 +0530

    File updated at 1/2/2026, 9:14:33 PM

--- 1-TwoSum/1-TwoSum.java
+++ 1-TwoSum/1-TwoSum.java
@@ -1,11 +1,15 @@
-class Solution {
-    public int[] twoSum(int[] nums, int target) {
-        for(int i = 0;i<nums.length;i++){
-            for(int j = i+1;j<nums.length;j++){
-                if(nums[i]+nums[j] == target)
-                return new int[] {i,j};
-            }
-        }
-            return new int[] {};        
-    }
-}
+// Last updated: 1/2/2026, 9:14:33 PM
+1class Solution {
+2    public int[] twoSum(int[] nums, int target) {
+3        int lengthOfNumsArray = nums.length;
+4
+5        for(int i = 0; i < lengthOfNumsArray; i++){
+6            for(int j = i+1; j < lengthOfNumsArray; j++){
+7                if(nums[i]+nums[j] == target){
+8                    return new int[]{i,j};
+9                }
+10            }
+11        }
+12        return new int[]{-1, -1};
+13    }
+14}
