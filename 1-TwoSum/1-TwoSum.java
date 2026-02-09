88b693f9f478a5197e0bcb1c89c7be6f11285560
Prabhu Teja <94112233+prabhu-tejaa@users.noreply.github.com>
2026-02-10 00:24:45 +0530

    File updated at 2/10/2026, 12:24:42 AM

--- 1-TwoSum/1-TwoSum.java
+++ 1-TwoSum/1-TwoSum.java
@@ -1,17 +1,17 @@
-// Last updated: 2/4/2026, 10:08:14 AM
+// Last updated: 2/10/2026, 12:24:42 AM
 1class Solution {
 2    public int[] twoSum(int[] nums, int target) {
-3        int sizeOfNums = nums.length;
-4        Map<Integer,Integer> map = new HashMap<>();
-5        for(int i = 0; i< sizeOfNums; i++){
-6            int num = nums[i];
-7            int difference = target - num;
+3        int numsLength = nums.length;
+4        Map<Integer, Integer> map = new HashMap<>();
+5
+6        for(int i = 0; i < numsLength; i++){
+7            int difference = target - nums[i];
 8
 9            if(map.containsKey(difference)){
 10                return new int[]{map.get(difference), i};
 11            }
 12
-13            map.put(num, i);
+13            map.put(nums[i], i);
 14        }
 15        return new int[]{};
 16    }
